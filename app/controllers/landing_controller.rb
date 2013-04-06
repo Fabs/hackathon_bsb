6f1b095616ecedabec194cbd1bd554edd829cb42
Fabricio Nascimento <fabriciosn@gmail.com>
2013-04-06 16:07:52 -0300

    Poisioning, fixing and displaying a search box

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,5 +1,5 @@
 class LandingController < ApplicationController
   def index
-    @settings = {zoom: 7}
+    @json = {map_options: {zoom: 3, center_latitude: -40.235004, center_longitude: -51.92528}}
   end
 end
