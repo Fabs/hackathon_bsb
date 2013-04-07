5ec868c9e7b03f13754758caaf824f8b4dd299dc
Fabricio Nascimento <fabriciosn@gmail.com>
2013-04-07 16:47:46 -0300

    Changing default place, and connection mongohq

--- app/controllers/map_routes_controller.rb
+++ app/controllers/map_routes_controller.rb
@@ -3,7 +3,7 @@ class MapRoutesController < ApplicationController
   include Geocoder
   def new
     @debug_origin = "Rua do Matão 1010, Cidade Universiatária, São Paulo"
-    @debug_destination = "Avenida Paulista 1400, São Paulo"
+    @debug_destination = "Parque do Ipiranga"
     @json = {map_options: {zoom: 3, center_latitude: -40.235004, center_longitude: -51.92528}}
   end
 
