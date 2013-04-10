978a441c82d8bea72af101197e8c3b9911e697e4
Fabricio Nascimento <fabriciosn@gmail.com>
2013-04-10 16:02:24 -0300

    fixed bugs to display online

--- app/controllers/map_routes_controller.rb
+++ app/controllers/map_routes_controller.rb
@@ -8,26 +8,26 @@ class MapRoutesController < ApplicationController
   end
 
   def create
-    School2.create_random if Poi.count == 0 #Initialize random schools
     @route = MapRoute.create_from_locations(params[:origin_location],params[:destination_location])
     
     redirect_to @route
   end
 
   def show
+    School.create_random if School.count == 0 #Initialize random schools    
     @route = MapRoute.find(params[:id])
     @json = MapRoute.find(params[:id]).route
   end
   
   def pois
-    @json = School2.all.to_gmaps4rails
+    @json = School.all.to_gmaps4rails
   end
   
   def near_route
     @boxes = params["_json"]
     @markers = []
     @boxes.each do |box|  
-      @markers += School2.within_box(location: box)
+      @markers += School.within_box(location: box)
     end
     respond_to do |format|
        format.json { render json: @markers.to_gmaps4rails }
