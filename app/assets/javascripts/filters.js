42a9689a33d670abd954301b5f3f26cd1fa21c2b
Fabricio Nascimento <fabriciosn@gmail.com>
2013-04-12 23:34:44 -0300

    Removing hiding markers

--- app/assets/javascripts/filters.js
+++ app/assets/javascripts/filters.js
@@ -1,5 +1,5 @@
 function install_filters(){
   for (var i=0; i<Gmaps.map.markers.length; i++) {
-    Gmaps.map.markers[i].serviceObject.setVisible(false);
+    //Gmaps.map.markers[i].serviceObject.setVisible(false);
   }
 }
